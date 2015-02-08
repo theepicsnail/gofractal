05298eef5fc1c615f4634f72a348e9c256eb491c
theepicsnail <theepicsnail@gmail.com>
2015-02-07 17:24:14 -0800

    cleanup and stuff

--- src/main.go
+++ src/main.go
@@ -5,32 +5,40 @@ import (
 	"fmt"
 	"image"
 	"image/color"
-	"math"
-	"path/filepath"
-	//"image/draw"
 	"image/png"
-	//"math"
-	//"math/cmplx"
+	"math"
 	"os"
+	"path/filepath"
 )
 
 const (
-	WIDTH  = 400
-	HEIGHT = 400
+	WIDTH  = 800
+	HEIGHT = 800
+
+	SCALE = 2 // How large of a coordinate system to fit in the image 2 = (-2 to 2)
+	XMIN  = -1 * SCALE
+	XMAX  = 1 * SCALE
+	YMIN  = -1 * SCALE
+	YMAX  = 1 * SCALE
+
+	X_DELTA = XMAX - XMIN
+	Y_DELTA = YMAX - YMIN
 )
 
+/* Utilities for point <-> pixel based on the above constants */
 func PixelToPoint(x, y int) (float64, float64) {
-	return float64(x*2)/float64(WIDTH) - 1,
-		float64(y*2)/float64(HEIGHT) - 1
+	return float64(x)*X_DELTA/float64(WIDTH) + XMIN,
+		float64(y)*Y_DELTA/float64(HEIGHT) + YMIN
 }
 
 func PointToPixel(x, y float64) (int, int) {
-	return int((x + 1) / 2 * WIDTH),
-		int((y + 1) / 2 * HEIGHT)
+	return int((x - XMIN) / X_DELTA * WIDTH),
+		int((y - YMIN) / Y_DELTA * HEIGHT)
 }
 
 func main() {
 
+	// setup
 	var percent *float64
 	var dir *string
 	percent = flag.Float64("p", 0, "")
@@ -38,54 +46,30 @@ func main() {
 	flag.Parse()
 	configure(*percent)
 
-	//Coefs[0].p = *percent
-	//Coefs[1].p = 1 - *percent
-
-	//Coefs[0].p = .1
-	//Coefs[1].p = .9
-
-	//Variations[0].w = *percent
-	//Variations[1].w = 1 - *percent
-
 	histogram := make([][]float64, HEIGHT)
 	for i := range histogram {
 		histogram[i] = make([]float64, WIDTH)
 	}
 
 	var max_val float64 = 0
-	/*for py := 0; py < HEIGHT; py++ {
-		for px := 0; px < WIDTH; px++ {
-			//p, ok := swirl[spherical[Point{x, y}]]
 
-			x, y := PointToPixel(
-				spherical(swirl(
-					spherical(swirl(
-						PixelToPoint(px, py))))))
-
-			if x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT {
-				continue
-			}
-
-			histogram[y][x] += 1
-			if histogram[y][x] > max_val {
-				max_val = histogram[y][x]
-			}
-		}
-	}*/
-
-	for iter := 0; iter < 100; iter++ {
-		x, y := rnd.Float64(), rnd.Float64() // TODO(snail) pick a random point for this.
-
-		for i := 0; i < 10000; i++ {
+	// is 100 different starting points any better than just 1 starting point?
+	for iter := 0; iter < 1; iter++ {
+		x, y := rnd.Float64(), rnd.Float64()
+		for i := 0; i < 10000000; i++ { // Points per random walk
 			x, y = randomFunction()(x, y)
+
 			if i > 20 {
-				// Plot
+				// Give the random walk time to get within a pixel of accurancy.
+
 				px, py := PointToPixel(x, y)
+				// TODO this check could be done before converting using px,py vs {X,Y}{MIN,MAX}
 				if px < 0 || py < 0 || px >= WIDTH || py >= HEIGHT {
 					continue
 				}
 
 				// Keep a running max
+				// Perhaps this could be quicker instead of 3*2 lookups
 				histogram[py][px] += 1
 				if histogram[py][px] > max_val {
 					max_val = histogram[py][px]
@@ -93,18 +77,18 @@ func main() {
 			}
 		}
 	}
+
 	// Create an image.
 	m := image.NewRGBA(image.Rect(0, 0, WIDTH, HEIGHT))
 
 	c := color.RGBA{255, 255, 255, 255}
 	// draw the pixels!
-	//for x := float64(m.Bounds().Min.X); x < float64(m.Bounds().Max.X); x++ {
-	//	for y := float64(m.Bounds().Min.Y); y < float64(m.Bounds().Max.Y); y++ {
 	for y := 0; y < HEIGHT; y++ {
 		for x := 0; x < WIDTH; x++ {
+			// fix brightness
 			c.R = uint8(math.Log(histogram[y][x]) * 255 / math.Log(max_val))
-			c.G = c.R
-			c.B = c.R
+			c.G = c.R/2 + c.R/4
+			c.B = 0 //c.R
 			m.Set(int(x), int(y), c)
 		}
 	}
