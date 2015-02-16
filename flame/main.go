52b0feec45bfa5749a2c061c6d1820d0fdb4ecf0
theepicsnail <theepicsnail@gmail.com>
2015-02-15 19:23:41 -0800

    Added profiler.

--- flame/main.go
+++ flame/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 import "github.com/theepicsnail/gofractal/flameutil"
 
 var FLAG_FILE = flag.String("file", "img%.2f.png", "Filename to save image as, use %.2f for percenage")
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
 	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	config := flameutil.NewFlameConfig()
 	configure(config)
 
